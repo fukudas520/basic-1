db27124f35b1ac0225632d8a520c4ec8f588b686
504a <moritaka@wdc-kd.com>
2021-04-02 14:49:50 +0900

    Initial commit

---
+++ src/hello/Hello.java
@@ -0,0 +1,13 @@
+package hello;
+
+public class Hello {
+    public static void main(String[] args) {
+        //これはコメントです
+        /*
+            これは
+            複数行の
+            コメント
+         */
+        System.out.println("こんにちは");
+    }
+}
